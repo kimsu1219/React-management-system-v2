17cda897676df28075c671c9a52f4ee3ffcc77a6
kimsu1219 <bstgom@naver.com>
2020-11-10 23:48:56 +0900

    git commit v2

--- client/src/App.js
+++ client/src/App.js
@@ -6,61 +6,81 @@ import Customer from './components/Customer'
 import CustomerTable from './components/CustomerTable'
 import { Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 // import classes from '*.module.css';
 
 const styles = theme => ({
   root: {
     width: "100%",
-    marginTop: theme.spacing.unit * 3,
+    // marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto"
   },
   table: {
     minWidth: 1080
-    }
+  },
+  progress: {
+    // margin: theme.spacing.unit * 2
+    margin: theme.spacing(2)
+  }
 });
+
 class App extends Component {
   // ?prop와 달리 변경될 수 있는 데이터를 담는 state?
   state = {
-    customers: ''
+    customers: '',
+    completed: 0
   }
     
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20); //progress함수를 계속실행시킬 timer
     this.callApi()
       .then(res => this.setState({customers: res})) //customer 목록데이터를 받아서 state로 설정, customers라는 변수 값에 body내용 담김
       .catch(err => console.log(err));
-    }
+  }
+  
+  componentWillMount() {
+    clearInterval(this.timer);
+  }
     
-    callApi = async () => {
+  callApi = async () => {
       const response = await fetch('/api/customers'); 
       const body = await response.json(); //custom 목록 json형태
       return body;
-    }
+  }
+  progress = () => { //circularprogress애니메이션을 위한 함수
+    const {completed} = this.state;
+    this.setState({completed: completed >= 100 ? 0 : completed + 1});
+  }
+
     
-    render() {
-      const { classes } = this.props;
-      return (
-        <Paper className={classes.root}>
-          <Table className={classes.table}>
-            <TableHead>
-              <TableRow>
-                <TableCell>번호</TableCell>
-                <TableCell>이미지</TableCell>
-                <TableCell>이름</TableCell>
-                <TableCell>생년월일</TableCell>
-                <TableCell>성별</TableCell>
-                <TableCell>직업</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.customers ? this.state.customers.map(c => {
-                return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-              }) : ''}
-            </TableBody>
-          </Table>
-        </Paper>
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {this.state.customers ? this.state.customers.map(c => {
+              // console.log(c)
+              return <CustomerTable key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+            }) : ""}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
+
 // const customers = [
 //   {
 //   'id': 1,
